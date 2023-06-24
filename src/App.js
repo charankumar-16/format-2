dff15ba97ce81fb3cdf51a12ed298cbafe327638
charan <charan.ku1605@gmail.com>
2023-06-24 19:53:39 +0000

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = { modifiedPhone: "" };
+
+  modifyPhoneNumber = (event) => {
+    const telInput = event.target;
+    const Number = telInput.value.replace(/\D/g, "").substring(0, 10);
+    const firstPart = Number.substring(0, 3);
+    const secondPart = Number.substring(3, 6);
+    const lastPart = Number.substring(6, 10);
+
+    if (Number.length > 6) {
+      const value = `(${firstPart}) ${secondPart}-${lastPart}`;
+      this.setState({ modifiedPhone: value });
+    } else if (Number.length > 3) {
+      const value = `(${firstPart}) ${secondPart}`;
+      this.setState({ modifiedPhone: value });
+    } else if (Number.length > 0) {
+      const value = `${firstPart}`;
+      this.setState({ modifiedPhone: value });
+    } else {
+      const value = "";
+      this.setState({ modifiedPhone: value });
+    }
+  };
+
+  render() {
+    const { modifiedPhone } = this.state;
+    return (
+      <div className="App">
+        <div className="container text-center">
+          <input
+            type="tel"
+            id="phone"
+            value={modifiedPhone}
+            length="16"
+            placeholder="mobile number"
+            autocomplete="off"
+            onChange={this.modifyPhoneNumber}
+          />
+          <div>
+            <label for="phone">(123) 456-7890</label>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
